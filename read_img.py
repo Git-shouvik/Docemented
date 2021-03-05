095595c77744274e6f3386e0954af991a8fcf0b5
Git-shouvik <65149605+Git-shouvik@users.noreply.github.com>
2021-03-05 16:07:04 +0530

    Update read_img.py

--- read_img.py
+++ read_img.py
@@ -2,6 +2,8 @@ import cv2 as cv
 import numpy as np
 img=cv.imread('alpa.jpg')
 cv.imshow("bits",img)
+
+#Convert RGB to Gray scale
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("Gray",gray)
 
